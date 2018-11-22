fe2ad95584d995364c70e0b8872d95d79c4fd52c
Naum Tuntev <tuntev@gmail.com>
2018-11-22 15:54:50 +0100

    Improved the design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import {Component, QueryList, ViewChildren} from '@angular/core';
 
-import { Platform } from '@ionic/angular';
+import {ActionSheetController, IonRouterOutlet, MenuController, ModalController, Platform, PopoverController} from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import {Router} from '@angular/router';
 
 @Component({
     selector: 'app-root',
@@ -22,10 +23,17 @@ export class AppComponent {
         }
     ];
 
+    @ViewChildren(IonRouterOutlet) routerOutlets: QueryList<IonRouterOutlet>;
+
     constructor(
         private platform: Platform,
         private splashScreen: SplashScreen,
-        private statusBar: StatusBar
+        private statusBar: StatusBar,
+        private router: Router,
+        public modalCtrl: ModalController,
+        private menu: MenuController,
+        private actionSheetCtrl: ActionSheetController,
+        private popoverCtrl: PopoverController
     ) {
         this.initializeApp();
 
@@ -35,6 +43,23 @@ export class AppComponent {
         this.platform.ready().then(() => {
             this.statusBar.styleBlackTranslucent();
             this.splashScreen.hide();
+
+            this.backButtonEvent();
+        });
+    }
+
+    // active hardware back button
+    backButtonEvent() {
+        this.platform.backButton.subscribe(async () => {
+
+            this.routerOutlets.forEach((outlet: IonRouterOutlet) => {
+                if (outlet && outlet.canGoBack()) {
+                    outlet.pop();
+
+                } else {
+                    navigator['app'].exitApp();
+                }
+            });
         });
     }
 }
