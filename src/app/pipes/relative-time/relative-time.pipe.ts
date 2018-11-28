e519476dda8a57db59c1c3d967f8b49836d07d6b
Naum Tuntev <tuntev@gmail.com>
2018-11-28 16:18:52 +0100

    Added timepipe

--- src/app/pipes/relative-time/relative-time.pipe.ts
+++ src/app/pipes/relative-time/relative-time.pipe.ts
@@ -1,5 +1,5 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import distanceInWordsToNow from 'date-fns/distance_in_words_to_now';
+import * as distanceInWordsToNow from 'date-fns/distance_in_words_to_now';
 
 @Pipe({
   name: 'relativeTime'
